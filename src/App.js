8ffc02657e26c9c2e61c075069b3499be5ffa8be
Tilio77 <tiliobolso1@gmail.com>
2022-12-05 23:17:19 -0300

    Navbar Finished

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import DotGroup from './scenes/DotGroup';
 import useMediaQuery from './hooks/useMediaQuery';
 import Navbar from "./scenes/Navbar";
+import Landing from './scenes/Landing';
 
 function App() {
 
   const [selectedPage, setSelectedPage] = useState("home");
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) setIsTopOfPage(true);
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    }
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, []);
   
   return (
     <div className="App bg-deep-blue">
-      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <Navbar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <div className="w-5/6 mx-auto md:h-full">
+        {isAboveMediumScreens && (
+          <DotGroup 
+            selectedPage = {selectedPage}
+            setSelectedPage = {setSelectedPage}
+          />
+        )}
+        <Landing setSelectedPage={setSelectedPage} />
+      </div>
     </div>
   );
 }
